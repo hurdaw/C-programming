842374acaa56f751c439b65216b0963bb1d156a7
hurdaw <tn133931@gmail.com>
2023-11-30 00:09:33 +0700

    eg

--- DSA/leetcode.cpp
+++ DSA/leetcode.cpp
@@ -334,6 +334,12 @@ public:
         }
         return result;
     }
+    // n=5
+    //  1
+    //  1 1
+    //  1 2 1
+    //  1 3 3 1
+    //  1 4 6 4 1  
     vector<vector<int>> generate(int numRows)
     {
         vector<vector<int>> result;
